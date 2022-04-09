879bb2b2c09fddb2f4bcc983f68bd44e36836291
Tirta Wirya Putra <tirtawiryaputra@yahoo.com>
2022-04-10 01:18:45 +0700

    :sparkles: Search Button done

--- src/routes/HomePage.js
+++ src/routes/HomePage.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Grid from '@mui/material/Grid';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import Divider from '@mui/material/Divider';
@@ -6,8 +7,21 @@ import Divider from '@mui/material/Divider';
 import { Item } from 'utils/Helper';
 import { SliderMarks } from 'components/SliderMarks';
 import { Input } from 'components/Input';
+import { CustomButton } from 'components/CustomButton';
 
 import BottomNav from 'components/BottomNav';
+
+const theme = createTheme({
+  breakpoints: {
+    values: {
+      mobile: 0,
+      tablet: 640,
+      laptop: 1024,
+      notebook: 1280,
+      desktop: 1440,
+    },
+  },
+});
 
 function HomePage() {
   const phone = useMediaQuery('(max-width:640px)');
@@ -22,55 +36,70 @@ function HomePage() {
     console.log(event.target.value, 'slider value');
     setSliderValue(event.target.value);
   }
-  // const handleClick = () => {
-  //   console.log('clicked');
-  // }
+  const handleClick = () => {
+    console.log('clicked');
+  }
 
   return (
-    <Grid
-      sx={{
-        color: '#FFFFFF',
-        flexDirection: 'column',
-      }}
-      container
-    >
-      <Grid item>
-        Search
-      </Grid>
-      <Input name={name} handleChange={handleChange}/>
-      <Divider sx={{ opacity: 0.1, border: '1px solid #FFFFFF'}} />
-      <Grid item sx={{ marginTop: '30px', marginBottom: '20px' }}>
-        {sliderValue} of results per page
-      </Grid>
+    <ThemeProvider theme={theme}>
       <Grid
-        container
         sx={{
-          display: 'flex',
-          alignItems: 'flex-end',
-        }}>
+          color: '#FFFFFF',
+          flexDirection: 'column',
+        }}
+        container
+      >
+        <Grid item>
+          Search
+        </Grid>
+        <Input name={name} handleChange={handleChange}/>
+        <Divider sx={{ opacity: 0.1, border: '1px solid #FFFFFF' }} />
+        <Grid item sx={{ marginTop: '30px', marginBottom: '20px' }}>
+          {sliderValue} of results per page
+        </Grid>
         <Grid
-          item
+          container
           sx={{
-            fontSize: '48px',
-            fontWeight: '700',
-            marginRight: '10px',
-            marginBottom: '30px',
+            display: 'flex',
+            alignItems: 'flex-end',
           }}>
-          <Item>30</Item>
+          <Grid
+            item
+            sx={{
+              fontSize: '48px',
+              fontWeight: '700',
+              marginRight: '10px',
+              marginBottom: '30px',
+            }}>
+            <Item>30</Item>
+          </Grid>
+          <Grid item sx={{ lineHeight: '24px', marginBottom: '30px',}}>
+            <Item>results</Item>
+          </Grid>
         </Grid>
-        <Grid item sx={{ lineHeight: '24px', marginBottom: '30px',}}>
-          <Item>results</Item>
-        </Grid>
+        <SliderMarks defaultValue={sliderValue} value={sliderValue} onChange={handleSlider} />
+        <Divider sx={{ opacity: 0.1, border: '1px solid #FFFFFF', marginTop: '30px' }} />
+        <CustomButton
+          onClick={handleClick}
+          sx={{
+            bottom: {
+              tablet: '87px',
+              mobile: '90px',
+            },
+            position: 'fixed',
+          }}
+        >
+          SEARCH
+        </CustomButton>
+        {
+            phone
+          ?
+            <BottomNav />
+          :
+            null
+        }
       </Grid>
-      <SliderMarks defaultValue={sliderValue} value={sliderValue} onChange={handleSlider} />
-      {
-          phone
-        ?
-          <BottomNav />
-        :
-          null
-      }
-    </Grid>
+    </ThemeProvider>
   );
 }
 
